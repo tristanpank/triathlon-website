99745c0c34ad4aecf889211947dc3d5d837b5e01
tristanpank <tristanpank@gmail.com>
2023-01-04 10:17:05 -0500

    Make basic REST api

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class WorkoutEntry(models.Model):
-  workout_type = models.CharField()
+  workout_type = models.CharField(max_length=30)
   time_of_workout = models.DateTimeField(auto_now_add=True)
   duration = models.DurationField()
   distance = models.IntegerField(default=0)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import WorkoutEntrySerializer
+from .models import WorkoutEntry
 # Create your views here.
+
+
+class WorkoutEntryView(generics.ListCreateAPIView):
+  queryset = WorkoutEntry.objects.all()
+  serializer_class = WorkoutEntrySerializer
